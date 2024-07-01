608e9629820053b94eb1f896918ad057b22fbbc6
1819s <1819sameerag@gmail.com>
2024-07-01 18:32:01 +0530

    node.js get method basics

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-let x = "4";
+const express = require('express')
+
+const app = express()
+
+
+function homePage (req, res){
+    res.send("Welcome to Homepage")
+}
+
+
+app.get('/', homePage )
+
+app.get('/user/:userId', (req, res) => {
+
+    let query = req.query;
+    let pathparams = req.params;
+
+    let object = {
+        query: query,
+        path: pathparams
+    }
+
+    res.send(object)
+
+} )
+
+
+app.listen(8080, () => {
+    console.log("Server is listening...")
+})
+
+
+
