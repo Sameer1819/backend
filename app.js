5c93b188b7d9be823dda1a57acdc63add2c05861
1819s <1819sameerag@gmail.com>
2024-07-02 18:05:38 +0530

    Added homework

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const mysql = require('mysql2')
+const bcrypt = require('bcrypt')
+const { STATUS_CODES } = require('http')
 
 const app = express()
-
-
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+
+
+
+
 
 
 function homePage (req, res){
@@ -20,6 +24,23 @@ const connection = mysql.createConnection({
     password: "Sameer@123",
     database: "development"
 })
+
+app.use((req, res, next) => {
+    if(req.method == "POST"){
+        const { email, password } = req.body;
+        if(password.length < 8){
+            res.status(400).send({
+                status: "FAILED",
+                reason: "Password length make greater than 8"
+            })
+        }
+        else{
+            next()
+        }
+    }
+})
+
+
 
 app.get('/', homePage )
 
@@ -36,26 +57,89 @@ app.get('/user/:userId', (req, res) => {
     res.send(object)
 } )
 
-app.post('/signup', async(req, res) => {
+app.post('/signup',  async(req, res) => {
 
     const body = req.body;
     const email = body.email;
     const password = body.password;
-    
+
     connection.query(
-        `SELECT user_id FROM users WHERE email="${email}"`, (err, result, fields) => {
+        `SELECT user_id FROM users WHERE email="${email}"`, async (err, result, fields) => {
             if(result.length === 0){
-                res.send("No user found you can signup, email: "+email+" password: "+password)
+                console.log("No user found you can signup, email: "+email+" password: "+password)
+                const hashedPw = await bcrypt.hash(password, 10);
+
+
+                connection.query(
+                    `INSERT INTO users (email, password) VALUES ("${email}", "${hashedPw}")`, (err, result, fields) => {
+                        if(err){
+                            res.status(400).send("Failed to Insert")
+                        }
+                        else{
+                            res.status(201).send({
+                                status: "SUCCESS",
+                                data: "User Created"
+                            });
+                        }
+                    }
+                )
             }
             else{
-                res.send("User exists go login")
+                res.send("User exists go login");
             }
         }
     );
 
 })
 
-hiii
+app.post('/login', async(req, res) =>{
+    const body=req.body
+    const email=body.email
+    const password=body.password
+
+    connection.query( `SELECT user_id FROM users WHERE email="${email}"`, async (err, result, fields) => {
+        if(result.length == 0){
+            console.log("No user found you can signup, email: "+email+" password: "+password)
+            res.status(400).send({
+                "STATUS":"FAILED",
+                "MESSAGE":"Email not Found, Please create account first"
+            })
+        }
+        //get password from DB
+        //compare
+        else{
+            connection.query( `SELECT password FROM users WHERE email="${email}"`, async (err, result, fields) => {
+                console.log(result)
+                const pwd=result[0].password
+                bcrypt.compare(password, pwd, function(err, result) {
+                    if(result==true){
+                        res.status(200).send({
+                            "STATUS":"SUCCESS",
+                            "MESSAGE":"LOGGED IN SUCCESSFULLY"
+                        })
+                    }
+                    else{
+                        res.status(403).send({
+                            "STATUS":"FAILED",
+                            "MESSAGE":"PASSWORD ENTERED IS WRONG"
+                        })
+                    }
+                });
+            })
+         
+        }
+    })
+})
+
+//{
+//    "email": "testUser@gmail.com",
+//1    "newEmail": "newmail@gmail.com"
+//1}
+app.post('/change-email', ) //for amar
+
+app.post('/reset-password') //for sameer
+
+
 app.listen(8080, () => {
     console.log("Server is listening...")
 })
